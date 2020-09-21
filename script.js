1fe71d7d3a086aa1b6ff9433df1f7741a5e1e683
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-09-21 13:54:08 -0300

    Initial upload.

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    const scoreDisplay = document.getElementById('score')
+    const width = 28
+}
