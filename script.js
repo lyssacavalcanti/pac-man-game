8dbac80c8f49a5f2d962751d22d7a3f07b429f2b
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-09-21 16:32:37 -0300

    Coding update.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid')
+    
     const scoreDisplay = document.getElementById('score')
     const width = 28
-
+    let score = 0
+    const grid = document.querySelector('.grid')
     const layout = [
         1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
         1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
@@ -33,4 +34,27 @@ document.addEventListener('DOMContentLoaded', () => {
         1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
         1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
       ]
+
+      // Legend: 0 = pac-dot/ 1 = wall / 2 = ghost-lair / 3 = power-pellet / 4 = empty
+      const squares =  []
+
+      function createBoard() {
+          for (let i=0; i < layout.length; i++) {
+              const square = document.createElement('div')
+              grid.appendChild(square)
+              squares.push(square)
+
+              if(layout[i] === 0) {
+                  squares[i].classList.add('pac-dot')
+              } else if (layout[i] === 1) {
+                  squares[i].classList.add('wall')
+              } else if (layout[i] === 2) {
+                  squares[i].classList.add('ghost-lair')
+              } else if (layout[i] === 3) {
+                  squares[i].classList.add('power-pellet')
+              }
+            }
+          }
+    createBoard()
+      
 })
